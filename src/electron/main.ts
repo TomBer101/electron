9225cfa68599e152d4cd1e5742567446436fda99
TomBer101 <tomerm1233@gmail.com>
2025-07-26 13:09:32 +0300

    Update main.ts
    Enforce singleton

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,10 +1,33 @@
-import { app, BrowserWindow, ipcMain } from 'electron'
+import { app, BrowserWindow } from 'electron'
 import path from 'path'
 import { isDev } from './util.js'
+import { NotesService } from './services/NotesService.js'
+import { FileDataSource } from './lib/dataSources/FileDataSource.js'
+import { NotesRepository } from './repositories/NotesRepository.js'
+import fs from 'fs'
+
+
+let mainWindow: BrowserWindow | null = null
+const gotTheLock = app.requestSingleInstanceLock()
+
+if(!gotTheLock) {
+    app.quit()
+} else {
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+        if(mainWindow) {
+            if(mainWindow.isMinimized()) {
+                mainWindow.restore()
+            }
+            mainWindow.focus()
+        } else {
+            createMainWindow()
+        }
+    })
+}
 
 
 function createMainWindow(): void {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 800,
         center: true,
@@ -22,8 +45,23 @@ function createMainWindow(): void {
     }
 }
 
+
+
+
 app.whenReady().then(() => {
+
+    //TODO: create here relevant services + file [athe & rpoes ]
     createMainWindow()
+
+    const userDataPath = app.getPath('userData')
+    const notesFilePath = path.join(userDataPath, 'notes.json')
+
+    if(!fs.existsSync(notesFilePath)) {
+        fs.writeFileSync(notesFilePath, '[]', 'utf-8')
+    } 
+
+    const notesRepository = new NotesRepository(new FileDataSource(notesFilePath))
+    const notesService = new NotesService(notesRepository)
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
