1cd85c3ea48d28f15aa54e5bea7f3a58215acb87
TomBer101 <tomerm1233@gmail.com>
2025-07-26 21:47:49 +0300

    Update main.ts

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -5,7 +5,10 @@ import { NotesService } from './services/NotesService.js'
 import { FileDataSource } from './lib/dataSources/FileDataSource.js'
 import { NotesRepository } from './repositories/NotesRepository.js'
 import fs from 'fs'
+import { ControllerManager } from './controllers/index.js'
 
+
+console.log('=== MAIN PROCESS STARTING ===')
 
 let mainWindow: BrowserWindow | null = null
 const gotTheLock = app.requestSingleInstanceLock()
@@ -35,33 +38,51 @@ function createMainWindow(): void {
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             contextIsolation: true,
+            nodeIntegration: false,
+            sandbox: false
         }
     })
 
+
+
+
     if (isDev()) {
+        
+    mainWindow.webContents.openDevTools()
+
         mainWindow.loadURL('http://localhost:5123')
     } else {
-        mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'))
+        mainWindow.loadFile(path.join(app.getAppPath(), '../../dist-react/index.html'))
     }
 }
 
-
-
-
-app.whenReady().then(() => {
-
-    //TODO: create here relevant services + file [athe & rpoes ]
-    createMainWindow()
-
+function initializeServices(): { notesService: NotesService } {
     const userDataPath = app.getPath('userData')
     const notesFilePath = path.join(userDataPath, 'notes.json')
 
     if(!fs.existsSync(notesFilePath)) {
         fs.writeFileSync(notesFilePath, '[]', 'utf-8')
-    } 
+    }
 
     const notesRepository = new NotesRepository(new FileDataSource(notesFilePath))
     const notesService = new NotesService(notesRepository)
+
+    return { notesService }
+}
+
+async function initializeControllers(notesService: NotesService): Promise<void> {
+    const { NotesController } = await import('./controllers/index.js')
+    const notesController = new NotesController(notesService)
+    const controllerManager = new ControllerManager(notesController)
+    controllerManager.registerHandlers()
+}
+
+
+app.whenReady().then(async () => {
+    const { notesService } = await initializeServices()
+    await initializeControllers(notesService)
+    createMainWindow()
+
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
