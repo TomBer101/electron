333c8a7a1733cee5e44376a48322764c1ffbdcb9
TomBer101 <tomerm1233@gmail.com>
2025-07-27 12:26:16 +0300

    Update preload.ts
    Added tags api

--- src/electron/preload.ts
+++ src/electron/preload.ts
@@ -1,6 +1,6 @@
 import { contextBridge, ipcRenderer } from 'electron'
 import { ElectronAPI } from '../shared/types.js'
-import { CreateNote, GetNotes, GetNoteById, UpdateNote, DeleteNote, TogglePinNote } from '../shared/types.js'
+import { CreateNote, GetNotes, GetNoteById, UpdateNote, DeleteNote, TogglePinNote, GetTags, CreateTag, DeleteTag } from '../shared/types.js'
 
 console.log('Preload script loaded')
 
@@ -10,7 +10,10 @@ const electronAPI: ElectronAPI = {
     createNote: (...args: Parameters<CreateNote>) => ipcRenderer.invoke('createNote', ...args),
     updateNote: (...args: Parameters<UpdateNote>) => ipcRenderer.invoke('updateNote', ...args),
     deleteNote: (...args: Parameters<DeleteNote>) => ipcRenderer.invoke('deleteNote', ...args),
-    togglePinNote: (...args: Parameters<TogglePinNote>) => ipcRenderer.invoke('togglePinNote', ...args)
+    togglePinNote: (...args: Parameters<TogglePinNote>) => ipcRenderer.invoke('togglePinNote', ...args),
+    getTags: (...args: Parameters<GetTags>) => ipcRenderer.invoke('getTags', ...args),
+    createTag: (...args: Parameters<CreateTag>) => ipcRenderer.invoke('createTag', ...args),
+    deleteTag: (...args: Parameters<DeleteTag>) => ipcRenderer.invoke('deleteTag', ...args)
 }
 
 console.log('Exposing electronAPI to window:', electronAPI)
