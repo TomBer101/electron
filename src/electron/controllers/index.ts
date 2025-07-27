52945ff9dfe722f40ab2753d3cbccb3d9121b421
TomBer101 <tomerm1233@gmail.com>
2025-07-27 12:29:40 +0300

    Update index.ts
    added tags

--- src/electron/controllers/index.ts
+++ src/electron/controllers/index.ts
@@ -1,15 +1,19 @@
 import { NotesController } from './notesController.js'
+import { TagsController } from './tagsController.js'
 
 export class ControllerManager {
     private notesController: NotesController
+    private tagsController: TagsController
 
-    constructor(notesController: NotesController) {
+    constructor(notesController: NotesController, tagsController: TagsController) {
         this.notesController = notesController
+        this.tagsController = tagsController
     }
 
     registerHandlers(): void {
         this.notesController.registerHandlers()
+        this.tagsController.registerHandlers()
     }
 }
 
-export { NotesController }
+export { NotesController, TagsController }
