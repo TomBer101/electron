a25985386a123d73e6b8940039d3788ac361f07a
TomBer101 <tomerm1233@gmail.com>
2025-07-27 12:30:07 +0300

    Update main.ts
    added tags

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -6,6 +6,8 @@ import { FileDataSource } from './lib/dataSources/FileDataSource.js'
 import { NotesRepository } from './repositories/NotesRepository.js'
 import fs from 'fs'
 import { ControllerManager } from './controllers/index.js'
+import { TagsRepository } from './repositories/tagsRepository.js'
+import { TagsService } from './services/TagsService.js'
 
 
 console.log('=== MAIN PROCESS STARTING ===')
@@ -43,22 +45,21 @@ function createMainWindow(): void {
         }
     })
 
-
-
-
     if (isDev()) {
-        
-    mainWindow.webContents.openDevTools()
-
+        mainWindow.webContents.openDevTools()
+        mainWindow.webContents.on('did-fail-load', (event, errorCode, errorDescription) => {
+            console.error('Failed to load URL:', errorCode, errorDescription)
+        })
         mainWindow.loadURL('http://localhost:5123')
     } else {
         mainWindow.loadFile(path.join(app.getAppPath(), '../../dist-react/index.html'))
     }
 }
 
-function initializeServices(): { notesService: NotesService } {
+function initializeServices(): { notesService: NotesService, tagsService: TagsService } {
     const userDataPath = app.getPath('userData')
     const notesFilePath = path.join(userDataPath, 'notes.json')
+    const tagsFilePath = path.join(userDataPath, 'tags.json')
 
     if(!fs.existsSync(notesFilePath)) {
         fs.writeFileSync(notesFilePath, '[]', 'utf-8')
@@ -67,20 +68,28 @@ function initializeServices(): { notesService: NotesService } {
     const notesRepository = new NotesRepository(new FileDataSource(notesFilePath))
     const notesService = new NotesService(notesRepository)
 
-    return { notesService }
+    if(!fs.existsSync(tagsFilePath)) {
+        fs.writeFileSync(tagsFilePath, '[]', 'utf-8')
+    }
+
+    const tagsRepository = new TagsRepository(new FileDataSource(tagsFilePath))
+    const tagsService = new TagsService(tagsRepository, notesService)
+
+    return { notesService, tagsService }
 }
 
-async function initializeControllers(notesService: NotesService): Promise<void> {
-    const { NotesController } = await import('./controllers/index.js')
+async function initializeControllers(notesService: NotesService, tagsService: TagsService): Promise<void> {
+    const { NotesController, TagsController } = await import('./controllers/index.js')
     const notesController = new NotesController(notesService)
-    const controllerManager = new ControllerManager(notesController)
+    const tagsController = new TagsController(tagsService)
+    const controllerManager = new ControllerManager(notesController, tagsController)
     controllerManager.registerHandlers()
 }
 
 
 app.whenReady().then(async () => {
-    const { notesService } = await initializeServices()
-    await initializeControllers(notesService)
+    const { notesService, tagsService } = await initializeServices()
+    await initializeControllers(notesService, tagsService)
     createMainWindow()
 
 
